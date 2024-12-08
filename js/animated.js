d928d4bc9b548ab8ff38a84356922008c5b18dcc
devtuanamit1409 <nguyentuanamit@gmail.com>
2024-12-08 11:32:41 +0700

    update

---
+++ js/animated.js
@@ -0,0 +1,23 @@
+ScrollReveal().reveal(".left-animated", {
+  duration: 500,
+  distance: "50px",
+  origin: "left",
+  easing: "ease-in-out",
+  delay: 200,
+});
+
+ScrollReveal().reveal(".bottom-animated", {
+  duration: 500,
+  distance: "50px",
+  origin: "bottom",
+  easing: "ease-in-out",
+  delay: 200,
+});
+
+ScrollReveal().reveal(".right-animated", {
+  duration: 500,
+  distance: "50px",
+  origin: "right",
+  easing: "ease-in-out",
+  delay: 200,
+});
